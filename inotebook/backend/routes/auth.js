9cd1836f7f233a2475b16627c1a38370905cb8e6
Mrajay04dev <ajay177081@gmail.com>
2024-01-03 11:35:03 +0530

    Catching Errors in Better Way

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -3,9 +3,9 @@ const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 
-// Create a User using : POST ||   "/api/'auth/".  Does'nt require auth
+// Create a User using : POST ||   "/api/'auth/signup".  No Login Required
 router.post(
-  "/",
+  "/signup",
   [
     body("email").isEmail().withMessage("Please enter a valid email address"),
     body("name")
@@ -14,24 +14,31 @@ router.post(
 
     body("password").isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    // If Errors Return Bad Request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    const newUser = User.create({
-      email: req.body.email,
-      name: req.body.name,
-      password: req.body.password,
-    })
-      .then((User) => res.json(User))
-      .catch((err) => {
-        console.log(err);
-        res.json({
-          error: "Please enter a Unique Email",
-          message: err.message,
-        });
+
+    // check weather the user exists or not
+    try {
+      let newUser = await User.findOne({ email: req.body.email });
+      if (newUser) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email  already exists" });
+      }
+      newUser = await User.create({
+        email: req.body.email,
+        name: req.body.name,
+        password: req.body.password,
       });
+      res.json(newUser);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some Error Occured");
+    }
   }
 );
 
