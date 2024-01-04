0b41e17453fb4d71a0b3c329e5ce2545548904b2
Mrajay04dev <ajay177081@gmail.com>
2024-01-04 16:24:01 +0530

    Jwt and Bcrypt Added

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const JWT_SECRET = "ajayisgoodboy1@";
+const jwt = require("jsonwebtoken");
 
 // Create a User using : POST ||   "/api/'auth/signup".  No Login Required
 router.post(
@@ -11,33 +14,44 @@ router.post(
     body("name")
       .isLength({ min: 3 })
       .withMessage("Username must be at least 5 characters long"),
-
     body("password").isLength({ min: 6 }),
   ],
   async (req, res) => {
-    // If Errors Return Bad Request and the errors
+    // Validation
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    // check weather the user exists or not
+    // Check if user with the email already exists
     try {
       let newUser = await User.findOne({ email: req.body.email });
       if (newUser) {
         return res
           .status(400)
-          .json({ error: "Sorry a user with this email  already exists" });
+          .json({ error: "Sorry, a user with this email already exists" });
       }
+
+      // Hash the password and create a new user
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
       newUser = await User.create({
         email: req.body.email,
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
       });
-      res.json(newUser);
+
+      // Generate JWT and send it in the response
+      const data = {
+        user: {
+          id: newUser.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Some Error Occured");
+      res.status(500).send("Some Error Occurred");
     }
   }
 );
