004eaaaee849f1bbc803ac6c6c5544889e7157c4
Mrajay04dev <ajay177081@gmail.com>
2024-01-04 16:58:12 +0530

    Authentication Done

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -3,10 +3,11 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
-const JWT_SECRET = "ajayisgoodboy1@";
 const jwt = require("jsonwebtoken");
 
-// Create a User using : POST ||   "/api/'auth/signup".  No Login Required
+const JWT_SECRET = "ajayisgoodboy1@";
+
+//Route:1 Create a User using: POST || "/api/auth/signup". No Login Required
 router.post(
   "/signup",
   [
@@ -26,6 +27,7 @@ router.post(
     // Check if user with the email already exists
     try {
       let newUser = await User.findOne({ email: req.body.email });
+
       if (newUser) {
         return res
           .status(400)
@@ -35,6 +37,7 @@ router.post(
       // Hash the password and create a new user
       const salt = await bcrypt.genSalt(10);
       const secPass = await bcrypt.hash(req.body.password, salt);
+
       newUser = await User.create({
         email: req.body.email,
         name: req.body.name,
@@ -52,6 +55,46 @@ router.post(
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some Error Occurred");
+    }
+  }
+);
+
+//Route:2 Authenticate a user using: POST "/api/auth/login". No Login Required
+router.post(
+  "/login",
+  [
+    body("email", "Please enter a valid email address").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let newUser = await User.findOne({ email });
+
+      if (!newUser) {
+        return res.status(400).json({ error: "Enter Correct Credentials" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, newUser.password);
+      if (!passwordCompare) {
+        return res.status(400).json({ error: "Enter Correct Credentials" });
+      }
+
+      const data = {
+        user: {
+          id: newUser.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json(authToken);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
     }
   }
 );
