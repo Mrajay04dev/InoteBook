88f8c793f30bd88c1dfab69b5a9035db2e3ee13e
Mrajay04dev <ajay177081@gmail.com>
2024-01-21 21:33:00 +0530

    Done

--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -3,9 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 connectToMongo();
-app.use(cors(
-  
-));
+app.use(cors());
 app.use(express.json());
 const port = 8080;
 // Available Routes
